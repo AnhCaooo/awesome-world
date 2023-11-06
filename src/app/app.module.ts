9e550d409dfdaf5bff194205f51707fd2aa8b2aa
Anh Cao <anhcao4922@gmail.com>
2023-11-06 12:27:38 +0700

    implement the fetch and display default data
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
 // Angular Materials
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 // COMPONENTS
 import { AppComponent } from './app.component';
@@ -24,9 +30,16 @@ import { NotfoundPageComponent } from './components/notfound-page/notfound-page.
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatTabsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { RestCountriesService } from 'src/app/services/rest-countries.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
 
+export class MainPageComponent implements OnInit {
+  countries: any[] = [];
+  loadingData = false;
+  
+  constructor (
+    private countryService: RestCountriesService,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.fetchCountries();
+  }
+
+  private fetchCountries() {
+    this.loadingData = true;
+    this.countryService.getCountries().subscribe({
+      next: (response: any) => {
+        this.countries = response
+      }, 
+      error: error => {
+        console.error('Error while fetching countries:', error)
+      }, 
+      complete: () => {
+        this.loadingData = false;
+      }
+    })
+  }
+
+  public parseLanguagesObjectToString(objectValue: any): string {
+    const returnValuesInSetType = new Set<string>([]);
+    if (this.isObject(objectValue)) {
+      for (const key in objectValue) {
+        returnValuesInSetType.add(objectValue[key]);
+      }
+    }
+    return this.convertSetToString(returnValuesInSetType);
+  }
+
+  public parseCurrenciesObjectToString(objectValue: any): string {
+    const returnValuesInSetType = new Set<string>([]);
+    if (this.isObject(objectValue)) {
+      for (const key in objectValue) {
+        returnValuesInSetType.add(`${objectValue[key].name} (${objectValue[key].symbol})`);
+      }
+    }
+    return this.convertSetToString(returnValuesInSetType);
+  }
+
+  private isObject(value: any): boolean {
+    return Object.prototype.toString.call(value) === '[object Object]';
+  }
+
+  private convertSetToString(setList: Set<string>): string {
+    return Array.from(new Set(setList)).join(", ");
+  }
 }
