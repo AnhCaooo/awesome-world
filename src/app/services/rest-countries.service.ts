e63458dc88b98bbcf50cd773b6b651655eb406a6
Anh Cao <anhcao4922@gmail.com>
2023-11-06 07:52:15 +0700

    implement service func to get country/ countries data
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

---
+++ src/app/services/rest-countries.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RestCountriesService {
+  private readonly restCountriesCoresUrl = 'https://restcountries.com';
+  private readonly urlVersion = 'v3.1';
+  private readonly findAll = 'all';
+  private readonly findByName = 'name';
+  private readonly findFullTextOption = 'fullText'
+  private readonly findByCode = 'alpha';
+  private readonly findByCurrency = 'currency';
+  private readonly findByLanguage = 'lang';
+  private readonly findByCapital = 'capital';
+  private readonly findByRegion = 'region';
+  private readonly findBySubregion = 'subregion';
+  private readonly findByTranslation = 'translation';
+  private readonly filter = '';
+
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+  /**
+   * 
+   * @param name - string. Name of country
+   * @param isFullName - boolean. Search by full name of that country or not.
+   * @returns finding country based on given parameters. If no parameters are given, search for all
+   */
+  getCountries(name?: string, isFullName?: boolean): Observable<any> {
+    let url = `${this.restCountriesCoresUrl}/${this.urlVersion}/`
+    if (name) {
+      url += `${this.findByName}/${name}`;
+
+      if (isFullName) {
+        url += `?${this.findFullTextOption}=${isFullName}`;
+      }
+    } else {
+      url += this.findAll;
+    }
+  
+    return this.http.get<any>(url);
+  }
+}
